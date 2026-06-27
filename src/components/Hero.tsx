bb60b057d8d2b5823df9e755f089ccd1856a12c2
madxip <jtaulats1976@gmail.com>
2026-06-27 14:05:24 +0200

    fix: resolve Hero vertical overlap and cut issues by implementing a single sliding container with 5 slots

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,23 +4,30 @@ export default function Hero() {
       <h1 className="hero-title">
         <div className="line-1">Troba</div>
 
-        {/* Contenidor de les paraules amb rotació CSS pura */}
+        {/* Contenidor de les paraules amb slider vertical en CSS pur */}
         <div className="words-container">
-          <div className="word-slide slide-1">
-            <div className="line-2">les millors</div>
-            <div className="line-3">extraescolars</div>
-          </div>
-          <div className="word-slide slide-2">
-            <div className="line-2">els millors</div>
-            <div className="line-3">tallers</div>
-          </div>
-          <div className="word-slide slide-3">
-            <div className="line-2">les millors</div>
-            <div className="line-3">activitats</div>
-          </div>
-          <div className="word-slide slide-4">
-            <div className="line-2">els millors</div>
-            <div className="line-3">casals</div>
+          <div className="words-slider">
+            <div className="word-slide">
+              <div className="line-2">les millors</div>
+              <div className="line-3">extraescolars</div>
+            </div>
+            <div className="word-slide">
+              <div className="line-2">els millors</div>
+              <div className="line-3">tallers</div>
+            </div>
+            <div className="word-slide">
+              <div className="line-2">les millors</div>
+              <div className="line-3">activitats</div>
+            </div>
+            <div className="word-slide">
+              <div className="line-2">els millors</div>
+              <div className="line-3">casals</div>
+            </div>
+            {/* Repetició de la primera per a un loop infinit i fluid */}
+            <div className="word-slide">
+              <div className="line-2">les millors</div>
+              <div className="line-3">extraescolars</div>
+            </div>
           </div>
         </div>
       </h1>
