be7f4880b00f067aee54cc3dbc8257e24ab2f0b0
madxip <jtaulats1976@gmail.com>
2026-07-30 11:49:13 +0200

    fix: treure import useState no utilitzat

--- src/components/Filtres.tsx
+++ src/components/Filtres.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useEffect, useState } from 'react';
+import { useMemo, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Activitat, Sponsor, CasalsBanner } from '@/lib/types';
